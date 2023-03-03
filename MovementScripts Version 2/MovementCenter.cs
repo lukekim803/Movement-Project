45471836d9837f7cb5c0769fe89167e8336f6e33
Luke Kim <105609017+lukekim803@users.noreply.github.com>
2023-03-03 12:02:39 -0500

    Add files via upload

---
+++ MovementScripts Version 2/MovementCenter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCenter : MonoBehaviour
+{
+
+    public float runspeed = 7;
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetMovingSpeed = runspeed;
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+        // Apply movement.
+        rb.velocity = transform.rotation * new Vector3(targetVelocity.x, rb.velocity.y, targetVelocity.y);
+    }
+}
+
